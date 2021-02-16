a8ec3388380f11ef18f569ea0f7de9f3eedc1d2e
r4ph3rd <hav4na@protonmail.com>
2021-02-16 12:53:23 +0100

    add slider component

---
+++ src/components/Slider.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+        <div class="pin">Today</div>
+    </div>
+</template>
+
+<style lang="scss" scope>
+.pin{
+    display:block;
+    background-color: $primary;
+    padding: 8px 20px;
+    border-radius:0 8px 8px 0;
+
+    text-align:left;
+    font-size: 14px;
+    font-weight: 600;
+    color:white;
+}
+    
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    
+      <v-slider></v-slider>
   </q-page>
 </template>
 
 <script>
+import Slider from '../components/Slider';
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components:{
+    'v-slider': Slider
+  }
 }
 </script>
